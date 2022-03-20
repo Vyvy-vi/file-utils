1b8a40453b9be7b5fbb2ce3f8d37cb8935aac0af
Vyvy-vi <vyom.j@protonmail.com>
2022-03-20 13:11:47 +0530

    feat: command-specific help message

--- src/commandHelpHeader.hpp
+++ src/commandHelpHeader.hpp
@@ -1,9 +1,59 @@
 // Command Help JSON Objects
-
+#include <iostream>
 #include "../include/json.hpp"
-
 using json = nlohmann::json;
-
+using std::cout;
+using std::endl;
+auto helpTextJson = R"(
+    {
+        "command": "file-utils",
+        "aliases": ["fu"],
+        "about": "A file utility CLI tool",
+        "usage": "<command> [<arg1>] ... [<argN>]",
+        "arguments": [
+            {
+                "identifier": "<command>",
+                "about": "The command to execute"
+            },
+            {
+                "identifier": "<arg>",
+                "about": "The arguments of the command"
+            }
+        ],
+        "global-options": [
+            {
+                "identifier": "-h",
+                "name": "command help",
+                "aliases": ["--help"],
+                "about": "Displays this help message"
+            },
+            {
+                "identifier": "-q",
+                "name": "quiet mode",
+                "aliases": ["--quiet"],
+                "about": "Doesn't ouput messages"
+            },
+            {
+                "identifier": "-v",
+                "name": "project version",
+                "aliases": ["--version"],
+                "about": "Displays the application's version"
+            },
+            {
+                "identifier": "--ansi",
+                "name": "ANSI mode",
+                "aliases": [],
+                "about": "Force ANSI output"
+            },
+            {
+                "identifier": "--no-ansi",
+                "name": "non-ANSI mode",
+                "aliases": [],
+                "about": "Disable ANSI output"
+            }
+        ]
+    }
+)"_json;
 auto commandsHelpJson = R"(
     {
         "help": {
@@ -19,8 +69,8 @@ auto commandsHelpJson = R"(
             "usage": "read <filename.ext>",
             "aliases": [],
             "about": "Reads a file and outputs contents to STDOUT",
-            "description": "",
-            "options": []
+            "description": "This command can be to read the contents of a file, and have them outputed to STDOUT or it can be piped to a new file",
+            "options" : []
         }
     }
 )"_json;
--- src/help.cpp
+++ src/help.cpp
@@ -20,60 +20,33 @@ void inline output_bold_text(string txt)
     cout << Style::Bold << txt << Style::Reset;
 }
 
+void printGlobalOptions()
+{
+    bool run;
+    for (json::iterator it = helpTextJson["global-options"].begin(); it != helpTextJson["global-options"].end(); ++it)
+    {
+        if (it == helpTextJson["global-options"].begin())
+        {
+            output_bold_text("Global Options");
+            cout << ": " << endl;
+            run = true;
+        }
+        string option_identifier = (*it)["identifier"].get<string>();
+        if ((*it)["aliases"].size() > 0)
+        {
+            option_identifier = option_identifier + " (" + (*it)["aliases"][0].get<string>() + ")";
+        }
+
+        cout << setw(MENU_SPACING) << "";
+        cout << setw(COLUMN_WIDTH) << left << option_identifier;
+        cout << (*it)["about"].get<string>() << endl;
+    }
+    if (run)
+        cout << endl;
+}
+
 void getHelp()
 {
-    cout << commandsHelpJson << endl;
-    auto helpTextJson = R"(
-        {
-            "command": "file-utils",
-            "aliases": ["fu"],
-            "about": "A file utility CLI tool",
-            "usage": "<command> [<arg1>] ... [<argN>]",
-            "arguments": [
-                {
-                    "identifier": "<command>",
-                    "about": "The command to execute"
-                },
-                {
-                    "identifier": "<arg>",
-                    "about": "The arguments of the command"
-                }
-            ],
-            "global-options": [
-                {
-                    "identifier": "-h",
-                    "name": "command help",
-                    "aliases": ["--help"],
-                    "about": "Displays this help message"
-                },
-                {
-                    "identifier": "-q",
-                    "name": "quiet mode",
-                    "aliases": ["--quiet"],
-                    "about": "Doesn't ouput messages"
-                },
-                {
-                    "identifier": "-v",
-                    "name": "project version",
-                    "aliases": ["--version"],
-                    "about": "Displays the application's version"
-                },
-                {
-                    "identifier": "--ansi",
-                    "name": "ANSI mode",
-                    "aliases": [],
-                    "about": "Force ANSI output"
-                },
-                {
-                    "identifier": "--no-ansi",
-                    "name": "non-ANSI mode",
-                    "aliases": [],
-                    "about": "Disable ANSI output"
-                }
-            ]
-        }
-    )"_json;
-
     output_bold_text("Usage");
     cout << ": " << helpTextJson["command"].get<string>() << " " << helpTextJson["usage"].get<string>() << endl
          << endl;
@@ -101,31 +74,17 @@ void getHelp()
     cout << endl
          << endl;
 
-    output_bold_text("Global Options");
-    cout << ": " << endl;
+    printGlobalOptions();
 
-    for (json::iterator it = helpTextJson["global-options"].begin(); it != helpTextJson["global-options"].end(); ++it)
-    {
-        string option_identifier = (*it)["identifier"].get<string>();
-        if ((*it)["aliases"].size() > 0)
-        {
-            option_identifier = option_identifier + " (" + (*it)["aliases"][0].get<string>() + ")";
-        }
-
-        cout << setw(MENU_SPACING) << "";
-        cout << setw(COLUMN_WIDTH) << left << option_identifier;
-        cout << (*it)["about"].get<string>() << endl;
-    }
-    cout << endl;
     output_bold_text("Commands");
     cout << ": " << endl;
     for (json::iterator it = commandsHelpJson.begin(); it != commandsHelpJson.end(); ++it)
     {
-
         cout << setw(MENU_SPACING) << "";
         cout << setw(COLUMN_WIDTH) << left << (*it)["name"].get<string>();
         cout << (*it)["about"].get<string>() << endl;
     }
+    cout << endl;
 }
 
 void getHelp(string cmd)
@@ -137,6 +96,41 @@ void getHelp(string cmd)
     }
     else
     {
-        cout << cmd << endl;
+        output_bold_text("Usage");
+        cout << ": " << helpTextJson["command"].get<string>() << " " << commandsHelpJson[cmd]["usage"].get<string>() << endl
+             << endl;
+
+        bool run;
+        for (json::iterator it = commandsHelpJson[cmd]["options"].begin(); it != commandsHelpJson[cmd]["options"].end(); ++it)
+        {
+            if (it == commandsHelpJson[cmd]["options"].begin())
+            {
+                output_bold_text("Options");
+                cout << ": " << endl;
+                run = true;
+            }
+            string option_identifier = (*it)["identifier"].get<string>();
+            if ((*it)["aliases"].size() > 0)
+            {
+                option_identifier = option_identifier + " (" + (*it)["aliases"][0].get<string>() + ")";
+            }
+
+            cout << setw(MENU_SPACING) << "";
+            cout << setw(COLUMN_WIDTH) << left << option_identifier;
+            cout << (*it)["about"].get<string>() << endl;
+        }
+        if (run)
+        {
+            cout << endl;
+        }
+        printGlobalOptions();
+        if ((commandsHelpJson[cmd].find("description") != commandsHelpJson[cmd].end()) && commandsHelpJson[cmd]["description"] != "")
+        {
+            output_bold_text("Description");
+            cout << ": " << endl;
+
+            cout << commandsHelpJson[cmd]["description"].get<string>() << endl
+                 << endl;
+        }
     }
 }
